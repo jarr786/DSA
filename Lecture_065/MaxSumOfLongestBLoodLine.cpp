1d62a758aeabfc32f18c811a9d98cb70bb47c251
TT <124626066+jarr2420@users.noreply.github.com>
2023-03-11 01:23:46 +0530

    Update MaxSumOfLongestBLoodLine.cpp

--- Lecture_065/MaxSumOfLongestBLoodLine.cpp
+++ Lecture_065/MaxSumOfLongestBLoodLine.cpp
@@ -1,3 +1,32 @@
+/*
+Given a binary tree of size N. Your task is to complete the function sumOfLongRootToLeafPath(), that find the sum of all nodes on the longest path from root to leaf node.
+If two or more paths compete for the longest path, then the path having maximum sum of nodes is being considered.
+
+Example 1:
+
+Input: 
+        4        
+       / \       
+      2   5      
+     / \ / \     
+    7  1 2  3    
+      /
+     6
+Output: 13
+Explanation:
+        4        
+       / \       
+      2   5      
+     / \ / \     
+    7  1 2  3 
+      /
+     6
+
+The highlighted nodes (4, 2, 1, 6) above are 
+part of the longest root to leaf path having
+sum = (4 + 2 + 1 + 6) = 13 */
+
+
 class Solution
 {
 public:
