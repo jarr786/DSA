6a254c630abd0c258cb8a4dc6ee37be1ff648f7a
TT <124626066+jarr786@users.noreply.github.com>
2023-06-16 08:35:06 +0530

    SudukuSolver.cpp

---
+++ Lecture_082-84 Backtracking/SudukuSolver.cpp
@@ -0,0 +1,80 @@
+
+/*
+37. Sudoku Solver
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+ 
+Example 1:
+
+Input: board = [["5","3",".",".","7",".",".",".","."],
+                ["6",".",".","1","9","5",".",".","."],
+                [".","9","8",".",".",".",".","6","."],
+                ["8",".",".",".","6",".",".",".","3"],
+                ["4",".",".","8",".","3",".",".","1"],
+                ["7",".",".",".","2",".",".",".","6"],
+                [".","6",".",".",".",".","2","8","."],
+                [".",".",".","4","1","9",".",".","5"],
+                [".",".",".",".","8",".",".","7","9"]]
+Output: [["5","3","4","6","7","8","9","1","2"],
+         ["6","7","2","1","9","5","3","4","8"],
+         ["1","9","8","3","4","2","5","6","7"],
+         ["8","5","9","7","6","1","4","2","3"],
+         ["4","2","6","8","5","3","7","9","1"],
+         ["7","1","3","9","2","4","8","5","6"],
+         ["9","6","1","5","3","7","2","8","4"],
+         ["2","8","7","4","1","9","6","3","5"],
+         ["3","4","5","2","8","6","1","7","9"]]
+         
+*/
+
+bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+    // row check
+    for(int i = 0; i < 9; i++) 
+		if(board[i][col] == c) 
+			return false;
+	// col check
+    for(int i = 0; i < 9; i++) 
+		if(board[row][i] == c) 
+			return false;
+    // box check
+    int x0 = (row/3) * 3, y0 = (col/3) * 3;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(board[x0 + i][y0 + j] == c) return false;
+        }
+    }
+    return true;
+}
+
+bool helper(vector<vector<char>> &board, int row, int col) {
+    // done
+    if(row == 9) 
+		return true;
+    // time for next row
+    if(col == 9) 
+		return helper(board, row + 1, 0);
+    // already marked
+    if(board[row][col] != '.') 
+		return helper(board, row, col + 1);
+
+    for(char c = '1'; c <= '9'; c++) {
+        if(isValid(board, row, col, c)) {
+            board[row][col] = c;
+            // without return here, the board reverts to initial state
+            if(helper(board, row, col + 1))
+				return true;
+            board[row][col] = '.';
+        }
+    }
+    return false;
+}
+
+void solveSudoku(vector<vector<char>>& board) {
+    helper(board, 0, 0);
+}
