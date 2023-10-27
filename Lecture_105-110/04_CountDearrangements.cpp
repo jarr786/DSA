bed07dfe53fe47f153bb69259b796dbc62778482
TT <124626066+jarr786@users.noreply.github.com>
2023-10-27 11:08:10 +0530

    Update 04_CountDearrangements.cpp

--- Lecture_105-110/04_CountDearrangements.cpp
+++ Lecture_105-110/04_CountDearrangements.cpp
@@ -15,7 +15,7 @@ long long int solve(int n,vector<long long int>&dp){
     if(n==1) return 0;
     if(n==2) return 1;
     if(dp[n]!=-1) return dp[n];
-     dp[n]=(((n-1)%MOD)*((solve(n-1,dp)%MOD)+(solve(n-2,dp)%MOD))%MOD);
+    dp[n]=(((n-1)%MOD)*((solve(n-1,dp)%MOD)+(solve(n-2,dp)%MOD))%MOD);
     return dp[n];
 }
 long long int countDerangements(int n) {
